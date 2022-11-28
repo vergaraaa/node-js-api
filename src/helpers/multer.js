fdf9eeebd3d1b0499dfcb6faaa092107dad4949c
Edgar Vergara <edgarvm13@hotmail.com>
2022-11-28 13:57:31 -0600

    changed multer destination route with path

--- src/helpers/multer.js
+++ src/helpers/multer.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { v4: uuid } = require('uuid');
 
 const storage = multer.diskStorage({
-    destination: path.join(__dirname, "/../public/uploads"),
+    destination: path.join(__dirname, "..", "public", "uploads"),
     filename: (req, file, cb) => {
         cb(null, uuid() + path.extname(file.originalname).toLocaleLowerCase());
     }
